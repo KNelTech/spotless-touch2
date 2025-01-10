9ae86fd95d95264108ff277a4681bc79e303eea3
footgun dev <kneldev@gmail.com>
2025-01-10 17:08:24 -0600

    shadow accordian

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import NavBar from './components/NavBar';
 import Billboard from './components/billboard';
-import Services from './components/services';
-import TextBox from './components/textBox';
+// import TextBox from './components/textBox';
 import AccordionUsage from './components/accordionService';
 import './App.css';
 import '@fontsource/roboto/300.css';
@@ -17,11 +16,10 @@ function App() {
       <Billboard />
       <main>
         <h1 className='aboutUsTitle'>About Us</h1>
-        <TextBox className="about" readOnly>
+        {/* <TextBox className="about" readOnly>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </TextBox>
+        </TextBox> */}
         <AccordionUsage />
-        <Services />
       </main>
     </div>
   );
--- src/components/accordionService/index.jsx
+++ src/components/accordionService/index.jsx
@@ -5,91 +5,102 @@ import AccordionActions from '@mui/material/AccordionActions';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import './accordian.css';
 
 const AccordionUsage = () => {
 return (
-    <div>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-            >
-                <Typography component="span">Fresh Start</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                Ideal for regular upkeep and a quick refresh <br />
-                Basic cleaning essentials (dusting, vacuuming, sweeping, and mopping) <br />
-                Kitchen counter cleaning <br />
-                Bathroom sanitization (toilet, sink, tub/shower) <br />
-                Pricing (Starting at): <br />
-                Small home (1–2 bedrooms): $125 <br />
-                Medium home (3–4 bedrooms): $150 <br />
-                Large home (5+ bedrooms): $200 <br />
-                Prices may vary for extra rooms or heavy clutter <br />
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2-content"
-                id="panel2-header"
-            >
-                <Typography component="span">Radiant Refresh</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                Go beyond the basics with detailed surface care <br />
-                Everything in Fresh Start, plus: <br />
-                Inside window cleaning (within safe reach) <br />
-                Baseboards and door-frame dusting <br />
-                Exterior appliance cleaning <br />
-                Garbage removal <br />
-                Pricing (Starting at): <br />
-                Small home (1–2 bedrooms): $150 <br />
-                Medium home (3–4 bedrooms): $225 <br />
-                Large home (5+ bedrooms): $300 <br />
-                Prices may vary for extra rooms or heavy clutter <br />
-            </AccordionDetails>
-        </Accordion>
-        <Accordion defaultExpanded>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3-content"
-                id="panel3-header"
-            >
-                <Typography component="span">Ultimate Sparkle</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                Comprehensive deep clean and organization for a spotless home <br />
-                Everything in Radiant Refresh, plus: <br />
-                Deep cleaning/organizing of kitchen and bathrooms <br />
-                Oven and refrigerator interior cleaning <br />
-                Closet organization <br />
-                Carpet deep cleaning <br />
-                Under-furniture cleaning <br />
-                Pricing (Starting at): <br />
-                Small home (1–2 bedrooms): $250 <br />
-                Medium home (3–4 bedrooms): $375 <br />
-                Large home (5+ bedrooms): $500 <br />
-                Prices may vary for extra rooms or heavy clutter <br />
-            </AccordionDetails>
-        </Accordion>
-        <Accordion>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel4-content"
-                id="panel4-header"
-            >
-                <Typography component="span">Add-On Services</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                (Optional, can be added to any package) <br />
-                Inside cabinet cleaning <br />
-                Carpet steam cleaning or upholstery cleaning <br />
-                Carpet deodorizing <br />
-                Additional tasks upon request <br />
-            </AccordionDetails>
-        </Accordion>
+    <div className="accordion-container">
+        <div>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Typography component="span">Fresh Start</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <ul className="accordianUL">
+                        <li>Ideal for regular upkeep and a quick refresh</li>
+                        <li>Basic cleaning essentials (dusting, vacuuming, sweeping, and mopping)</li>
+                        <li>Kitchen counter cleaning</li>
+                        <li>Bathroom sanitization (toilet, sink, tub/shower)</li>
+                        <li>Pricing (Starting at):</li>
+                        <li>Small home (1–2 bedrooms): $125</li>
+                        <li>Medium home (3–4 bedrooms): $150</li>
+                        <li>Large home (5+ bedrooms): $200</li>
+                        <li>Prices may vary for extra rooms or heavy clutter</li>
+                    </ul>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    <Typography component="span">Radiant Refresh</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <ul className="accordianUL">
+                        <li>Go beyond the basics with detailed surface care</li>
+                        <li>Everything in Fresh Start, plus:</li>
+                        <li>Inside window cleaning (within safe reach)</li>
+                        <li>Baseboards and door-frame dusting</li>
+                        <li>Exterior appliance cleaning</li>
+                        <li>Garbage removal</li>
+                        <li>Pricing (Starting at):</li>
+                        <li>Small home (1–2 bedrooms): $150</li>
+                        <li>Medium home (3–4 bedrooms): $225</li>
+                        <li>Large home (5+ bedrooms): $300</li>
+                        <li>Prices may vary for extra rooms or heavy clutter</li>
+                    </ul>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3-content"
+                    id="panel3-header"
+                >
+                    <Typography component="span">Ultimate Sparkle</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <ul className="accordianUL">
+                        <li>Comprehensive deep clean and organization for a spotless home</li>
+                        <li>Everything in Radiant Refresh, plus:</li>
+                        <li>Deep cleaning/organizing of kitchen and bathrooms</li>
+                        <li>Oven and refrigerator interior cleaning</li>
+                        <li>Closet organization</li>
+                        <li>Carpet deep cleaning</li>
+                        <li>Under-furniture cleaning</li>
+                        <li>Pricing (Starting at):</li>
+                        <li>Small home (1–2 bedrooms): $250</li>
+                        <li>Medium home (3–4 bedrooms): $375</li>
+                        <li>Large home (5+ bedrooms): $500</li>
+                        <li>Prices may vary for extra rooms or heavy clutter</li>
+                    </ul>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel4-content"
+                    id="panel4-header"
+                >
+                    <Typography component="span">Add-On Services</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <ul className="accordianUL">
+                        <li>(Optional, can be added to any package)</li>
+                        <li>Inside cabinet cleaning</li>
+                        <li>Carpet steam cleaning or upholstery cleaning</li>
+                        <li>Carpet deodorizing</li>
+                        <li>Additional tasks upon request</li>
+                    </ul>
+                </AccordionDetails>
+            </Accordion>
+        </div>
     </div>
 );
 };
