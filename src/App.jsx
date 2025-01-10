112dddb5a08f312bb10740eeefefa083d0427a41
Oscar Arias <oarias@pop-os.localdomain>
2025-01-09 18:14:41 -0600

    created initial services boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import NavBar from './components/NavBar';
 import Billboard from './components/billboard';
+import Services from './components/services';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Billboard />
-      <main style={{ padding: '20px' }}>
+      <Services />
+      {/* <main style={{ padding: '20px' }}>
         <h2>Welcome to Spotless Touch!</h2>
         <p>Your trusted cleaning solution in Chicago.</p>
-      </main>
+      </main> */}
     </div>
   );
 }
