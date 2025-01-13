21f48f23095bd8f02caccfb945fc3438987e7ab9
Oscar Arias <oarias@pop-os.localdomain>
2025-01-13 12:31:03 -0600

    added google recaptcha v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import App from './App';
 import './index.css';
+
+const RECAPTCHA_SITE_KEY = "6LclmLUqAAAAAP4DC3Ru-GJJLQ2So3NRVKAq6Sai";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
       <App />
+    </GoogleReCaptchaProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
